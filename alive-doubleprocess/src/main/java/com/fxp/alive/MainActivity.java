dc8ab1587d8cb382a474834d8f0192158afd4876
fangxiaopeng <850899969@qq.com>
2019-03-27 10:07:22 +0800

    commit module alive-doubleprocess

---
+++ alive-doubleprocess/src/main/java/com/fxp/alive/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fxp.alive;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.fxp.alive.doubleprocess.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
